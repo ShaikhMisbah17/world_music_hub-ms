2b300cba49dd3a7f0c3fa0387fdc3bd802f0cb48
MISBAH SHAIKH <smisbah62@gmail.com>
2022-09-16 00:01:12 +0530

    Changes made to navbar for tablet view

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,17 +6,19 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar__wrapper">
-        <div className="navbar__logo">
-          <img
-            src="https://wmh.ai/static/media/logo.de2bf2d4.svg"
-            alt="app__logo"
-          />
-        </div>
+        <div className="navbar__left-section">
+          <div className="navbar__logo">
+            <img
+              src="https://wmh.ai/static/media/logo.de2bf2d4.svg"
+              alt="app__logo"
+            />
+          </div>
 
-        <div className="navbar__logo-name">
-          <h2>
-            World <span className="yellow-text">Music</span> Hub
-          </h2>
+          <div className="navbar__logo-name">
+            <h2>
+              World <span className="yellow-text">Music</span> Hub
+            </h2>
+          </div>
         </div>
 
         <div className="navbar__right-section">
@@ -28,18 +30,18 @@ const Navbar = () => {
           </div>
 
           <ul className="app__navbar-links">
-            <li className="p__opensans">
+            <li>
               <a className="explore" href="#explore">
                 Explore
               </a>
             </li>
-            <li className="p__opensans">
+            <li>
               <a className="about-us" href="#aboutus">
                 About Us
               </a>
             </li>
-            <li className="p__opensans">
-              <a className="explore" href="#login">
+            <li>
+              <a className="login" href="#login">
                 Login
               </a>
             </li>
